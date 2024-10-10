8a6a2bbaebaf4a25aa6a1e9686cf8111cd919148
Jiayi <jc224@imperial.ac.uk>
2024-10-10 17:57:17 +0800

    write python script

--- week2/code/dictionary.py
+++ week2/code/dictionary.py
@@ -12,15 +12,23 @@ taxa = [ ('Myotis lucifugus','Chiroptera'),
 
 # Write a python script to populate a dictionary called taxa_dic derived from
 # taxa so that it maps order names to sets of taxa and prints it to screen.
-# 
-# An example output is:
-#  
-# 'Chiroptera' : set(['Myotis lucifugus']) ... etc. 
-# OR, 
-# 'Chiroptera': {'Myotis  lucifugus'} ... etc
 
-#### Your solution here #### 
+# Initialize an empty dictionary
+taxa_dic = {}
 
-# Now write a list comprehension that does the same (including the printing after the dictionary has been created)  
- 
-#### Your solution here #### 
+# Loop through the taxa and make a dictionary
+for species, order in taxa:
+    if order not in taxa_dic:
+        taxa_dic[order] = set()
+    taxa_dic[order].add(species)
+
+# Print
+for order, species in taxa_dic.items():
+    print(f"'{order}': {species}")
+
+# A list comprehension that does the same (including the printing after the dictionary has been created)  
+taxa_dic = {order: {species for species, o in taxa if o == order} for species, order in taxa}
+
+# Print
+for order, species in taxa_dic.items():
+    print(f"'{order}': {species}")
