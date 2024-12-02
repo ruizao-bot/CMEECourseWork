e6b86d76b1bbc534a844704739d20f5a7b70edb3
Jiayi <jc224@imperial.ac.uk>
2024-12-03 06:54:29 +0800

    modify_2

--- week4/code/Florida.R
+++ week4/code/Florida.R
@@ -39,15 +39,16 @@ p_value <- mean(correlations_permuted >= correlation_origin)
 print(paste("P-value from permutation analysis:", p_value))
 
 # visualization
-h <- ggplot(as.data.frame(correlations_permuted), aes(x=correlations_permuted)) +
-  geom_histogram(colour="white", fill="blue") +
-  geom_vline(xintercept = correlation_origin, colour = "red", size=1) +
-  labs(x="Correlation Coefficient", y="Frequency") +
-  xlim(-0.4,0.6) +
+h <- ggplot(mapping = aes(x = correlations_permuted)) +
+  geom_histogram(colour = "white", fill = "blue") +
+  geom_vline(xintercept = correlation_origin, colour = "red", size = 1) +
+  labs(x = "Correlation Coefficient", y = "Frequency") +
+  xlim(-0.4, 0.6) +
   theme_classic() +
   theme(
     axis.title.x = element_text(size = 14),
-    axis.title.y = element_text(size = 14) 
+    axis.title.y = element_text(size = 14)
   )
+
 
 ggsave("../results/histogram_plot.png", plot = h, width = 8, height = 6, dpi = 100)
